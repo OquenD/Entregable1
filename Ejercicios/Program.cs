ca24913fb7277721ebb816e56683e4538bb43ed6
OquenD <oquendodavid45@gmail.com>
2022-08-17 18:54:56 -0500

    base

--- Ejercicios/Program.cs
+++ Ejercicios/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! and some more");
+﻿Console.WriteLine("Ingrese la evaluacion -(0.0)(0.4)(0.6) ");
+double puntaje = double.Parse(Console.ReadLine());
+string [] nivel = {"Inaceptable","Aceptable","Meritorio"};
+double [] pun = {0.0,0.4,0.6};
+int i = 0;
+int posicion = 0;
+
+if(puntaje!=0.0 || puntaje!=0.4 || puntaje!=0.6){
+    Console.WriteLine("Ingrese una evaluacion valida");
+    Environment.Exit(0);
+}
+else
+{
+while(i<=2){
+if (puntaje == pun[i]){
+  posicion=i;
+  i=3;   
+}
+i++;
+}
+}
+Console.WriteLine($"Su nivel de evaluacion es de {nivel[posicion]}");
+Console.WriteLine($"La bonificacion ganada es de {puntaje*50000} ");
