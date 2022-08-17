8664501ccff064797970b76f21931b5be2485d05
OquenD <oquendodavid45@gmail.com>
2022-08-17 18:14:15 -0500

    Completado

--- Ejercicios/Program.cs
+++ Ejercicios/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Ingrese tu nombre completo ");
+string nombre = Console.ReadLine();
+Console.WriteLine("Ingrese tu edad");
+uint edad = uint.Parse(Console.ReadLine());
+
+int precio = 15000;
+int [] age = {18,4,0};
+double [] discount = {0.03,0.05,1.0};
+int i = 0;
+int posicion = 0;
+
+while(i<=2){
+if (edad > age[i]){
+  posicion=i;
+  i=3;   
+}
+i++;
+}
+
+Console.WriteLine($"Bienvenid@ {nombre}");
+Console.WriteLine($"Tu precio de entrada es de ${precio-(precio*discount[posicion])}");
+Console.WriteLine($"Tu descuento fue de {discount[posicion]*100}% que equivale a: ${(precio*discount[posicion])}");
