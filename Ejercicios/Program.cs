93f33062555db6db85ca97efd43f703833ce545a
OquenD <oquendodavid45@gmail.com>
2022-08-17 17:59:50 -0500

    Completado

--- Ejercicios/Program.cs
+++ Ejercicios/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("");
+﻿Console.WriteLine("Ingrese los valores del primer semestre ");
+uint semestre1 = uint.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese los valores del Segundo semestre ");
+uint semestre2 = uint.Parse(Console.ReadLine());
+
+uint anual = semestre1 + semestre2;
+
+int [] renta = {45000,35000,20000,10000,0};
+double [] impuesto = {0.3,0.20,0.15,0.10,0.05};
+int i = 0;
+int posicion = 0;
+
+while(i<=4){
+if (anual > renta[i]){
+  posicion=i;
+  i=5;   
+}
+i++;
+}
+
+Console.WriteLine($"Su renta anual es de {anual}");
+Console.WriteLine($"Tiene que pagar un impuesto de {impuesto[posicion]*100}% que equivale a ${anual*impuesto[posicion]} ");
+Console.WriteLine($"Para un total de ${anual+(anual*impuesto[posicion])}");
