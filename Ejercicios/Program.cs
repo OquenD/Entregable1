dc38fc7bcb66ab1e4ad424624d6525a30bc3760d
OquenD <oquendodavid45@gmail.com>
2022-08-17 23:01:46 -0500

    Completado

--- Ejercicios/Program.cs
+++ Ejercicios/Program.cs
@@ -1,23 +1,20 @@
-﻿Console.WriteLine("Ingrese la evaluacion -(0.0)(0.4)(0.6) ");
+﻿Console.WriteLine("Ingrese la evaluacion (0.0)(0.4)(0.6) ");
 double puntaje = double.Parse(Console.ReadLine());
-string [] nivel = {"Inaceptable","Aceptable","Meritorio"};
-double [] pun = {0.0,0.4,0.6};
-int i = 0;
-int posicion = 0;
 
-if(puntaje!=0.0 || puntaje!=0.4 || puntaje!=0.6){
-    Console.WriteLine("Ingrese una evaluacion valida");
-    Environment.Exit(0);
+switch (puntaje){
+  case 0.0:
+    Console.WriteLine("Su nivel de evaluacion es de Inaceptable");
+    Console.WriteLine($"La bonificacion ganada es de {0.0*50000}");
+  break;
+  case 0.4:
+    Console.WriteLine("Su nivel de evaluacion es de Aceptable");
+    Console.WriteLine($"La bonificacion ganada es de {0.4*50000}");
+  break;
+  case 0.6:
+    Console.WriteLine("Su nivel de evaluacion es de Meritorio");
+    Console.WriteLine($"La bonificacion ganada es de {0.6*50000}");
+  break;
+  default:
+    Console.WriteLine("Evaluacion no valida");
+  break;
 }
-else
-{
-while(i<=2){
-if (puntaje == pun[i]){
-  posicion=i;
-  i=3;   
-}
-i++;
-}
-}
-Console.WriteLine($"Su nivel de evaluacion es de {nivel[posicion]}");
-Console.WriteLine($"La bonificacion ganada es de {puntaje*50000} ");
